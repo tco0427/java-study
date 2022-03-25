6bf54a8385d908fc466a833dd6605756fc49946b
Donggyu <a29661498@gmail.com>
2022-03-25 12:01:42 +0900

    feat: sumOverThreeAndDouble() 구현

--- src/main/java/techcourse/fp/mission/StreamStudy.java
+++ src/main/java/techcourse/fp/mission/StreamStudy.java
@@ -34,7 +34,10 @@ public class StreamStudy {
     }
 
     public static long sumOverThreeAndDouble(List<Integer> numbers) {
-        return 0L; // TODO: 이 부분을 구현한다.
+        return numbers.stream()
+                .filter(number -> number > 3)
+                .mapToLong(number -> (number * 2))
+                .sum();
     }
 
     public static void printLongestWordTop100() throws IOException {
